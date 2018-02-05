87b664d95dd1d9a78102da9a216631bd039f6c66
026 <026@192.168.0.64>
2018-02-05 20:29:42 +0900

    데이터 타입

---
+++ ch03_datatype/src/com/inc/example/NumberEx1.java
@@ -0,0 +1,31 @@
+package com.inc.example;
+
+public class NumberEx1 {
+
+	public static void main(String[] args) {
+		byte byte1 = -128;
+		System.out.println(byte1);
+		
+		//byte byte2 = -129;
+		//byte byte3 = 128;
+
+		//byte2는 -128보다 작은 정수는 저장할 수 없다.
+		
+		
+		short short1 = 128;
+		System.out.println(short1);
+		short short2 = 32767;
+		//short short3 = -32769;
+		
+		
+		//실습 : int에 저장 가능한 최대 숫자를 변수에 저장
+		
+		int int1 = 2147483647;
+		int int2 = -2147483648;
+		
+		
+		
+
+	}
+
+}
