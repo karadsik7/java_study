25041360e71692020a719143edb532a5e1d1335e
026 <026@192.168.0.64>
2018-02-21 17:33:23 +0900

    콜렉션

---
+++ ch27_collections/src/com/inc/list/ArrayListEx1.java
@@ -0,0 +1,25 @@
+package com.inc.list;
+
+import java.util.ArrayList;
+
+public class ArrayListEx1 {
+	
+	public static void main(String[] args) {
+		
+		ArrayList<String> seats = new ArrayList<>();
+		seats.add("박태환");
+		seats.add("김연아");
+		seats.add("손연재");
+		seats.add("윤성빈");
+		System.out.println(seats.get(0));
+		for(int i = 0; i < seats.size(); i++) {
+			System.out.printf("%s\t", seats.get(i));
+		}
+		
+		
+		
+		
+		
+	}
+
+}
