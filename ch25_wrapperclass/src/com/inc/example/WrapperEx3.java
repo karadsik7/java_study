cf2f75b49f94df1064240291d71cd70e65269b82
026 <026@192.168.0.64>
2018-02-22 17:49:49 +0900

    랩퍼 클래스

--- ch25_wrapperclass/src/com/inc/example/WrapperEx3.java
+++ ch25_wrapperclass/src/com/inc/example/WrapperEx3.java
@@ -14,9 +14,10 @@ public class WrapperEx3 {
 		
 		Person p3 = new Person();
 		p3.setName("윤성빈");
-		p3.setAge(new Age(30, 29));
+		p3.setAge(new Age(30, 29)); //오브젝트로 Age클래스객체가 형변환
 		
-		int manAge = ((Age)p3.getAge()).man;
+		int manAge = ((Age)p3.getAge()).man; //오브젝트에서 Age 객체를 다시 사용가능하도록 Age로 형변환
+		//그러나 처리우선순위가 .이 더 높으므로 형변환을 먼저 처리하도록 괄호로 우선순위를 부여함
 		System.out.println(manAge);
 		
 		
